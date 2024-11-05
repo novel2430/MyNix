507bd15513b2ee5598202902e8655d3bd68e8f29
novel2430 <novel2430@163.com>
2024-11-05 16:15:11 +0800

    remove modify pkg: hmcl, openttd

--- modify-pkgs/default.nix
+++ modify-pkgs/default.nix
@@ -1,7 +1,5 @@
 { pkgs, unstable-pkgs, nix23-pkgs, ... }:
 {
-  hmcl = (import ./hmcl.nix { pkgs=pkgs; }).hmcl;
-  openttd = (import ./openttd.nix { pkgs=pkgs; }).openttd;
   brave = (import ./brave.nix { 
     pkgs = pkgs; 
     unstable-pkgs = unstable-pkgs;
