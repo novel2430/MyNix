ffddb8954628950d7c561d48e3aa1d4e352662ed
novel2430 <novel2430@163.com>
2024-11-06 15:26:15 +0800

    update riverwm config

--- home/modules/river/init.nix
+++ home/modules/river/init.nix
@@ -1,214 +1,195 @@
-{pkgs, ...}:
-{
+{ pkgs, lib, ... }:
+with lib;
+let
+  # Define paths to custom and standard applications
+  apps = {
+    foot = "${pkgs.foot}/bin/foot";
+    brightnessctl = "${pkgs.brightnessctl}/bin/brightnessctl";
+    wofi = "${pkgs.wofi}/bin/wofi";
+    wlrootsClipboard = "wlroot-clipboard";
+    swaylock = "my-swaylock";
+    powerMenu = "wofi-power-menu";
+    volume = "my-volume";
+    screenshot = "grim-slurp-screenshot";
+    browser = "brave --enable-features=UseOzonePlatform --ozone-platform=wayland --gtk-version=4";
+  };
+  # Define keybindings
+  keybindings = {
+    basic = [
+      # Close focused view
+      { modes = ["normal"]; keys = "Super Q"; action = "close"; }
+      # Exit river
+      { modes = ["normal"]; keys = "Super+Shift E"; action = "exit"; }
+      # Layout navigation
+      { modes = ["normal"]; keys = "Super J"; action = "focus-view next"; }
+      { modes = ["normal"]; keys = "Super K"; action = "focus-view previous"; }
+      { modes = ["normal"]; keys = "Super+Shift J"; action = "swap next"; }
+      { modes = ["normal"]; keys = "Super+Shift K"; action = "swap previous"; }
+      { modes = ["normal"]; keys = "Super+Shift Return"; action = "zoom"; }
+      { modes = ["normal"]; keys = "Super H"; action = "send-layout-cmd rivercarro 'main-ratio -0.05'"; }
+      { modes = ["normal"]; keys = "Super L"; action = "send-layout-cmd rivercarro 'main-ratio +0.05'"; }
+      # Fullscreen and floating toggle
+      { modes = ["normal"]; keys = "Super F"; action = "toggle-fullscreen"; }
+      { modes = ["normal"]; keys = "Super Space"; action = "toggle-float"; }
+      # Change Layout
+      { modes = ["normal"]; keys = "Super M"; action = "send-layout-cmd rivercarro 'main-location monocle'"; }
+      { modes = ["normal"]; keys = "Super+Shift M"; action = "send-layout-cmd rivercarro 'main-location left'"; }
+    ];
+    apps = [
+      # Terminal
+      { modes = ["normal"]; keys = "Super Return"; action = "${apps.foot}"; }
+      # Custom application bindings
+      { modes = ["normal"]; keys = "Super D"; action = "${apps.wofi} --show drun"; }
+      { modes = ["normal"]; keys = "Super R"; action = "${apps.wofi} --show run"; }
+      { modes = ["normal"]; keys = "Super+Shift F"; action = "${apps.browser}"; }
+      { modes = ["normal"]; keys = "Super C"; action = "${apps.wlrootsClipboard}"; }
+      { modes = ["normal"]; keys = "Super+Shift L"; action = "${apps.swaylock} manual"; }
+      { modes = ["normal"]; keys = "Super+Shift P"; action = "${apps.powerMenu}"; }
+      # Volume control
+      { modes = ["normal" "locked"]; keys = "None XF86AudioRaiseVolume"; action = "${apps.volume} up"; }
+      { modes = ["normal" "locked"]; keys = "None XF86AudioLowerVolume"; action = "${apps.volume} down"; }
+      { modes = ["normal" "locked"]; keys = "None XF86AudioMute"; action = "${apps.volume} mute"; }
+      # Brightness control
+      { modes = ["normal" "locked"]; keys = "None XF86MonBrightnessUp"; action = "${apps.brightnessctl} set +10%"; }
+      { modes = ["normal" "locked"]; keys = "None XF86MonBrightnessDown"; action = "${apps.brightnessctl} set 10%-"; }
+      # Screenshot
+      { modes = ["normal" "locked"]; keys = "None Print"; action = "${apps.screenshot} full"; }
+      { modes = ["normal"]; keys = "Super Print"; action = "${apps.screenshot} select"; }
+      # Reload Config
+      { modes = ["normal"]; keys = "Super+Shift R"; action = "~/.config/river/init reload"; }
+    ];
+  };
+  # Define View Rules
+  view-rules = {
+    # Force Floating
+    floating = [
+      {app-id = "float*"; title = "foo";}
+    ];
+    # Force Csd
+    csd = [
+      {app-id = "bar";}
+    ];
+  };
+  # Define Gaps
+  gaps = {
+    smart-gap = false;
+    inner = "10";
+    outer = "20";
+  };
+  # Define Keyboard Repeat Rate
+  keyboard-repeat = {
+    rate = "50";
+    delay = "300";
+  };
+  # Define Tags Operation
+  tags = {
+    count = 7;
+    keybindings = {
+      set-focused-tags = "Super";
+      set-view-tags = "Super+Shift";
+      toggle-focused-tags = "Super+Control";
+      all-tags = "Super 0";
+    };
+  };
+  # Define Background and Border
+  background-border-settings = {
+    background-color = "0x002b36";
+    border-color-focused = "0xd8dee9";
+    border-color-unfocused = "0x3b4252";
+    border-width = "4";
+  };
+
+
+  # Helper function to generate commands
+  pow = x: y:
+    if y == 0 then 1 else x * pow x (y - 1);
+  formatKeybindingBasic = binding: 
+    concatStringsSep "\n" (builtins.map (mode: "riverctl map ${mode} ${binding.keys} ${binding.action}") binding.modes);
+  formatKeybindingApps = binding: 
+    concatStringsSep "\n" (builtins.map (mode: "riverctl map ${mode} ${binding.keys} spawn '${binding.action}'") binding.modes);
+  formatBackgroundBorderSettings = k: v: 
+    "riverctl ${k} ${v}";
+  formatViewRulesFloat = binding:
+    (
+      if binding ? title then
+        "riverctl rule-add -app-id '${binding.app-id}' -title '${binding.title}' float"
+      else
+        "riverctl rule-add -app-id '${binding.app-id}' float"
+    );
+  formatViewRulesCsd = binding:(
+    if binding ? title then
+      "riverctl rule-add -app-id '${binding.app-id}' -title '${binding.title}' csd"
+    else
+      "riverctl rule-add -app-id '${binding.app-id}' csd"
+  );
+  formatGaps = binding:(
+    if binding.smart-gap == true then
+      "rivercarro -outer-gaps ${binding.outer} -inner-gaps ${binding.inner} &\n"
+    else
+      "rivercarro -no-smart-gaps -outer-gaps ${binding.outer} -inner-gaps ${binding.inner} &\n"
+  );
+  formatKeyboardRepeat = binding:(
+    "riverctl set-repeat ${binding.rate} ${binding.delay}"
+  );
+  formatTags = binding: count: (
+    let
+      generate = count: 
+        let
+          str-count = builtins.toString count;
+          tags = builtins.toString (pow 2 (count - 1));
+        in
+          if count > 0 then
+            [''
+              riverctl map normal ${binding.keybindings.set-focused-tags} ${str-count} set-focused-tags ${tags}
+              riverctl map normal ${binding.keybindings.set-view-tags} ${str-count} set-view-tags ${tags}
+              riverctl map normal ${binding.keybindings.toggle-focused-tags} ${str-count} toggle-focused-tags ${tags}
+            '']
+            ++ generate (count - 1) 
+          else [];
+    in
+      concatStringsSep "\n" (generate count)
+  );
+  formatShowAllTags = binding: (
+    ''
+      all_tags=$(((1 << 32) - 1))
+      riverctl map normal ${binding.keybindings.all-tags} set-focused-tags $all_tags
+    ''
+  );
+
+in {
   home.file.".config/river/init" = {
-    text = with pkgs; ''
+    text = ''
       #!/bin/sh
 
-      # This is the example configuration file for river.
-      #
-      # If you wish to edit this, you will probably want to copy it to
-      # $XDG_CONFIG_HOME/river/init or $HOME/.config/river/init first.
-      #
-      # See the river(1), riverctl(1), and rivertile(1) man pages for complete
-      # documentation.
+      # River configuration
+      # Basic
+      ${concatStringsSep "\n" (builtins.map formatKeybindingBasic keybindings.basic)}
+      # Apps
+      ${concatStringsSep "\n" (builtins.map formatKeybindingApps keybindings.apps)}
 
-      # Note: the "Super" modifier is also known as Logo, GUI, Windows, Mod4, etc.
+      # Background and Border configuration
+      ${concatStringsSep "\n" (mapAttrsToList formatBackgroundBorderSettings background-border-settings)}
 
-      # Super+Shift+Return to start an instance of foot (https://codeberg.org/dnkl/foot)
-      riverctl map normal Super Return spawn ${foot}/bin/foot
+      # View Rules
+      # Floating
+      ${concatStringsSep "\n" (builtins.map formatViewRulesFloat view-rules.floating)}
+      # Csd
+      ${concatStringsSep "\n" (builtins.map formatViewRulesCsd view-rules.csd)}
 
-      # Super+Q to close the focused view
-      riverctl map normal Super Q close
-
-      # Super+Shift+E to exit river
-      riverctl map normal Super+Shift E exit
-
-      # Super+J and Super+K to focus the next/previous view in the layout stack
-      riverctl map normal Super J focus-view next
-      riverctl map normal Super K focus-view previous
-
-      # Super+Shift+J and Super+Shift+K to swap the focused view with the next/previous
-      # view in the layout stack
-      riverctl map normal Super+Shift J swap next
-      riverctl map normal Super+Shift K swap previous
-
-      # Super+Period and Super+Comma to focus the next/previous output
-      # riverctl map normal Super Period focus-output next
-      # riverctl map normal Super Comma focus-output previous
-
-      # Super+Shift+{Period,Comma} to send the focused view to the next/previous output
-      # riverctl map normal Super+Shift Period send-to-output next
-      # riverctl map normal Super+Shift Comma send-to-output previous
-
-      # Super+Shift+Return to bump the focused view to the top of the layout stack
-      riverctl map normal Super+Shift Return zoom
-
-      # Super+H and Super+L to decrease/increase the main ratio of rivertile(1)
-      riverctl map normal Super H send-layout-cmd rivercarro "main-ratio -0.05"
-      riverctl map normal Super L send-layout-cmd rivercarro "main-ratio +0.05"
-
-      # Super+Shift+H and Super+Shift+L to increment/decrement the main count of rivertile(1)
-      # riverctl map normal Super+Shift H send-layout-cmd rivertile "main-count +1"
-      # riverctl map normal Super+Shift L send-layout-cmd rivertile "main-count -1"
-
-      # Super+Alt+{H,J,K,L} to move views
-      # riverctl map normal Super+Alt H move left 100
-      # riverctl map normal Super+Alt J move down 100
-      # riverctl map normal Super+Alt K move up 100
-      # riverctl map normal Super+Alt L move right 100
-
-      # Super+Alt+Control+{H,J,K,L} to snap views to screen edges
-      # riverctl map normal Super+Alt+Control H snap left
-      # riverctl map normal Super+Alt+Control J snap down
-      # riverctl map normal Super+Alt+Control K snap up
-      # riverctl map normal Super+Alt+Control L snap right
-
-      # Super+Alt+Shift+{H,J,K,L} to resize views
-      # riverctl map normal Super+Alt+Shift H resize horizontal -100
-      # riverctl map normal Super+Alt+Shift J resize vertical 100
-      # riverctl map normal Super+Alt+Shift K resize vertical -100
-      # riverctl map normal Super+Alt+Shift L resize horizontal 100
-
-      # Super + Left Mouse Button to move views
-      riverctl map-pointer normal Super BTN_LEFT move-view
-
-      # Super + Right Mouse Button to resize views
-      riverctl map-pointer normal Super BTN_RIGHT resize-view
-
-      # Super + Middle Mouse Button to toggle float
-      riverctl map-pointer normal Super BTN_MIDDLE toggle-float
-
-      for i in $(seq 1 7)
-      do
-          tags=$((1 << ($i - 1)))
-
-          # Super+[1-9] to focus tag [0-8]
-          riverctl map normal Super $i set-focused-tags $tags
-
-          # Super+Shift+[1-9] to tag focused view with tag [0-8]
-          riverctl map normal Super+Shift $i set-view-tags $tags
-
-          # Super+Control+[1-9] to toggle focus of tag [0-8]
-          riverctl map normal Super+Control $i toggle-focused-tags $tags
-
-          # Super+Shift+Control+[1-9] to toggle tag [0-8] of focused view
-          riverctl map normal Super+Shift+Control $i toggle-view-tags $tags
-      done
-
-      # Super+0 to focus all tags
-      # Super+Shift+0 to tag focused view with all tags
-      all_tags=$(((1 << 32) - 1))
-      riverctl map normal Super 0 set-focused-tags $all_tags
-      # riverctl map normal Super+Shift 0 set-view-tags $all_tags
-
-      # Super+Space to toggle float
-      riverctl map normal Super Space toggle-float
-
-      # Super+F to toggle fullscreen
-      riverctl map normal Super F toggle-fullscreen
-
-      # Super+{Up,Right,Down,Left} to change layout orientation
-      # riverctl map normal Super Up    send-layout-cmd rivertile "main-location top"
-      # riverctl map normal Super Right send-layout-cmd rivertile "main-location right"
-      # riverctl map normal Super Down  send-layout-cmd rivertile "main-location bottom"
-      # riverctl map normal Super Left  send-layout-cmd rivertile "main-location left"
-
-      # Declare a passthrough mode. This mode has only a single mapping to return to
-      # normal mode. This makes it useful for testing a nested wayland compositor
-      # riverctl declare-mode passthrough
-
-      # Super+F11 to enter passthrough mode
-      # riverctl map normal Super F11 enter-mode passthrough
-
-      # Super+F11 to return to normal mode
-      # riverctl map passthrough Super F11 enter-mode normal
-
-      # Various media key mapping examples for both normal and locked mode which do
-      # not have a modifier
-      for mode in normal locked
-      do
-          # Eject the optical drive (well if you still have one that is)
-          riverctl map $mode None XF86Eject spawn 'eject -T'
-
-          # Control pulse audio volume with pamixer (https://github.com/cdemoulins/pamixer)
-          riverctl map $mode None XF86AudioRaiseVolume  spawn 'my-volume up'
-          riverctl map $mode None XF86AudioLowerVolume  spawn 'my-volume down'
-          riverctl map $mode None XF86AudioMute         spawn 'my-volume mute'
-
-          # Control MPRIS aware media players with playerctl (https://github.com/altdesktop/playerctl)
-          # riverctl map $mode None XF86AudioMedia spawn 'playerctl play-pause'
-          # riverctl map $mode None XF86AudioPlay  spawn 'playerctl play-pause'
-          # riverctl map $mode None XF86AudioPrev  spawn 'playerctl previous'
-          # riverctl map $mode None XF86AudioNext  spawn 'playerctl next'
-
-          # Control screen backlight brightness with brightnessctl (https://github.com/Hummer12007/brightnessctl)
-          riverctl map $mode None XF86MonBrightnessUp   spawn '${brightnessctl}/bin/brightnessctl set +10%'
-          riverctl map $mode None XF86MonBrightnessDown spawn '${brightnessctl}/bin/brightnessctl set 10%-'
-
-          # ScreenShot
-          riverctl map $mode None Print spawn 'grim-slurp-screenshot full'
-          riverctl map $mode Super Print spawn 'grim-slurp-screenshot select'
-
-      done
-
-      # Set background and border color
-      riverctl background-color 0x002b36
-      riverctl border-color-focused 0xd8dee9
-      riverctl border-color-unfocused 0x3b4252
-      riverctl border-width 4
+      # Tags Operation
+      ${formatTags tags tags.count}
+      ${formatShowAllTags tags}
 
       # Set keyboard repeat rate
-      riverctl set-repeat 50 300
+      ${formatKeyboardRepeat keyboard-repeat}
 
-      # Make all views with an app-id that starts with "float" and title "foo" start floating.
-      riverctl rule-add -app-id 'float*' -title 'foo' float
-
-      # Make all views with app-id "bar" and any title use client-side decorations
-      riverctl rule-add -app-id "bar" csd
-
-      # Set the default layout generator to be rivertile and start it.
-      # River will send the process group of the init executable SIGTERM on exit.
-      # riverctl default-layout rivertile
-      # rivertile -view-padding 6 -outer-padding 6 &
-
-      # Add others rivercarro's commands the same way with the keybinds you'd like.
-      # e.g.
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "inner-gaps -1"
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "inner-gaps +1"
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "outer-gaps -1"
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "outer-gaps +1"
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "width-ratio -0.1"
-      # riverctl map normal $mod <keys> send-layout-cmd rivercarro "width-ratio +0.1"
-
-      riverctl map normal Super M send-layout-cmd rivercarro "main-location monocle"
-      riverctl map normal Super+Shift M send-layout-cmd rivercarro "main-location left"
-
-      # Set the default layout generator to be rivercarro and start it.
-      # River will send the process group of the init executable SIGTERM on exit.
+      # Default layout and autostart
       riverctl default-layout rivercarro
-      rivercarro -no-smart-gaps -outer-gaps 20 -inner-gaps 10 &
-      
-      # Custom Key Binding
-      ## Reload RiverWM <Super+Shiht+R>
-      riverctl map normal Super+Shift R spawn "~/.config/river/init reload"
-      ## Wofi Desktop Menu
-      riverctl map normal Super D spawn "${wofi}/bin/wofi --show drun"
-      ## Wofi Run Menu
-      riverctl map normal Super R spawn "${wofi}/bin/wofi --show run"
-      ## Brave Browser
-      riverctl map normal Super+Shift F spawn "brave -enable-features=UseOzonePlatform -ozone-platform=wayland --gtk-version=4"
-      ## Clipboard
-      riverctl map normal Super C spawn "wlroot-clipboard"
-      ## Lock Screen
-      riverctl map normal Super+Shift L spawn "my-swaylock manual"
-      ## Power Menu
-      riverctl map normal Super+Shift P spawn "wofi-power-menu"
+      # Gaps
+      ${formatGaps gaps}
 
-
-      # Autostart Script
-      if [ $# -eq 0 ]
-        then
-          my-river-autostart
+      if [ $# -eq 0 ]; then
+        my-river-autostart
       fi
     '';
     executable = true;
