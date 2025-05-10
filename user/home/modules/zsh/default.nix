ce95bec41c9c939519435cd72cbf271e99ccf594
novel2430 <novel2430@163.com>
2025-05-10 14:59:43 +0800

    update

--- user/home/modules/zsh/default.nix
+++ user/home/modules/zsh/default.nix
@@ -24,49 +24,58 @@
         zstyle ":completion:*" list-colors ''${(s.:.)LS_COLORS} 
 
         autoload -Uz vcs_info
-        precmd() { vcs_info }
+        preexec() {
+          print -Pn "\e]0;$1\a"
+        }
+        precmd() { 
+          vcs_info 
+          print -Pn "\e]0;%n@%m:%~\a"
+          
+        }
         setopt prompt_subst
-        # 启用 git 支持
         zstyle ':vcs_info:*' enable git
-        # 显示修改状态（如 + 有 staged 改动，! 有 unstaged）
         zstyle ':vcs_info:git:*' check-for-changes true
         zstyle ':vcs_info:git:*' stagedstr '%F{green}+%f'
         zstyle ':vcs_info:git:*' unstagedstr '%F{yellow}!%f'
         zstyle ':vcs_info:git:*' formats '(%F{red}%b%f)%u%c '
 
-        PROMPT='%B%F{green}[%n:%F{blue}%~%F{green}]$%f%b ''${vcs_info_msg_0_}'
-
-        export EDITOR="nvim"
-        export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=245"
-        export AWESOMEWM_BAR_CLIENT_TITLE_MAX_LEN="${opt-config.awesomewm-bar-client-title-max-len}"
-        export XDG_CACHE_HOME="/home/${opt-config.username}/.cache"
+        export PROMPT='%B%F{green}[%n:%F{blue}%~%F{green}]$%f%b ''${vcs_info_msg_0_}'
       '')
       # FZF Stuff
       (''
-        export FZF_COMPLETION_TRIGGER='\'
         # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
         # - The first argument to the function ($1) is the base path to start traversal
         # - See the source code (completion.{bash,zsh}) for the details.
         _fzf_compgen_path() {
           fd --follow --exclude ".git" . "$1"
         }
-
         # Use fd to generate the list for directory completion
         _fzf_compgen_dir() {
           fd --type d --follow --exclude ".git" . "$1"
         }
       '')
-      # JAVA
-      (
-      ''
-        export JAVA_17_HOME="${pkgs.jdk17}/bin"
-        export JAVA_21_HOME="${pkgs.jdk21}/bin"
-      ''
-      )
+      (lib.mkIf (opt-config.use-proxy == true) ''
+        alias nixos-rebuild="sudo http_proxy=${opt-config.http-proxy} https_proxy=${opt-config.https-proxy} nixos-rebuild"
+      '')
+    ];
+    profileExtra = lib.mkMerge [
       (lib.mkIf (opt-config.use-proxy == true) ''
         export http_proxy="${opt-config.http-proxy}"
         export https_proxy="${opt-config.https-proxy}"
-        alias nixos-rebuild="sudo http_proxy=${opt-config.http-proxy} https_proxy=${opt-config.https-proxy} nixos-rebuild"
+      '')
+      # JAVA
+      (''
+        export JAVA_17_HOME="${pkgs.jdk17}/bin"
+        export JAVA_21_HOME="${pkgs.jdk21}/bin"
+      '')
+      (''
+        export FZF_COMPLETION_TRIGGER='\'
+      '')
+      (''
+        export EDITOR="nvim"
+        export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=245"
+        export AWESOMEWM_BAR_CLIENT_TITLE_MAX_LEN="${opt-config.awesomewm-bar-client-title-max-len}"
+        export XDG_CACHE_HOME="/home/${opt-config.username}/.cache"
       '')
     ];
   };
