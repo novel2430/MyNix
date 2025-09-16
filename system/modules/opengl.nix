24e92a36aed436422af946b3a6ea03664a859cc3
novel2430 <novel2430@163.com>
2025-09-17 00:18:12 +0800

    update

--- system/modules/opengl.nix
+++ system/modules/opengl.nix
@@ -2,7 +2,13 @@
 {
   # OpenGL
   hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      intel-media-driver    # HD4000 建议也装上，VA-API/视频输出更稳
+      vaapiIntel
+      libvdpau-va-gl
+    ];
   };
+
 }
