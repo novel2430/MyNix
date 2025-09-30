79f123239e1f6540ae79e0ba21f72adca4bf7d3a
novel2430 <novel2430@163.com>
2025-09-30 12:46:07 +0800

    update

--- user/home/modules/niri/default.nix
+++ user/home/modules/niri/default.nix
@@ -2,6 +2,7 @@
 {
   home.file.".config/niri/waybar.jsonc".source = ./waybar.jsonc;
   home.file.".config/niri/waybar.css".source = ./waybar.css;
+  home.file.".config/niri/config.kdl".source = ./config.kdl;
   home.packages = with pkgs; [
     niri 
     xwayland-satellite
